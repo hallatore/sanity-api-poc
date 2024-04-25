52a6bbcdc0a38e5a53ed20ba3834a808be26a7ed
Bendik Bjørndal Iversen <bendik.iversen@moller.no>
2024-04-25 10:17:38 +0200

    feat: Use product images from app1/main in app2

--- app2/next.config.js
+++ app2/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     poweredByHeader: false,
     generateEtags: false,
     productionBrowserSourceMaps: true,
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'cdn.sanity.io', pathname: '/**' },
+        ],
+    },
 };
 
 module.exports = nextConfig;
